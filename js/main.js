4d109b8e155fce223205618535ce37d9fcdf109b
Teodoro Salgado Mac <tsalgado96@gmail.com>
2017-04-24 16:02:07 -0400

    Made the navbar sticky and added bootstraps scrollspy.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 $(function() {
+
+  /*SMOOTH SCROLL*/
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 60
         }, 1000);
         return false;
       }
     }
   });
 
+  /*ANIMATE PROGRESS BAR*/
   var waypoint = new Waypoint({
   element: document.getElementsByClassName('progress-bar'),
   handler: function(direction) {
@@ -22,6 +25,7 @@ $(function() {
     offset: '90%'
   });
 
+  /*FULLSCREEN CONTACT MODAL*/
   $(".modal-fullscreen").on('show.bs.modal', function () {
     setTimeout( function() {
       $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
@@ -31,5 +35,19 @@ $(function() {
     $(".modal-backdrop").addClass("modal-backdrop-fullscreen");
   });
 
+  /*SCROLLSPY*/
+  $(function() {
+    //caches a jQuery object containing the header element
+    var header = $("#main-navigation");
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 5) {
+            header.addClass("navbar-bg");
+        } else {
+            header.removeClass("navbar-bg");
+        }
+    });
+  });
 
 });
